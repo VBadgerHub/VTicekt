5d63a35b2c7301b98451c0e53216f946b757f3e6
Vorczu <adlyko@o2.pl>
2023-03-05 06:43:41 +0100

    first commit

---
+++ src/App.ts
@@ -0,0 +1,27 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+
+const users = [
+    {
+        id: 1,
+        name: 'Adam'
+    },
+    {
+        id: 2,
+        name: 'Władek'
+    }
+]
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(users);
+});
+
+app.listen(port, () => {
+  console.log(` 🔥 [server]: Server działa na http://localhost:${port}`); 
+});  
